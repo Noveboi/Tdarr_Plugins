07906c68fd866435022cf3fefc79be415c33d60a
George Nikolaidis <nove@novesoftware.net>
2026-06-26 21:05:34 +0300

    Add `CodecType` enum

--- FlowPluginsTs/FlowHelpers/1.0.0/nove/ffmpeg.ts
+++ FlowPluginsTs/FlowHelpers/1.0.0/nove/ffmpeg.ts
@@ -2,8 +2,18 @@
 import { checkFfmpegCommandInit } from '../interfaces/flowUtils';
 import { IpluginDetails, IpluginInputArgs, IpluginOutputArgs } from '../interfaces/interfaces';
 
+export const CodecType = {
+  VIDEO: 'video',
+  AUDIO: 'audio',
+  SUBTITLE: 'subtitle',
+} as const;
+
+export type CodecType = typeof CodecType[keyof typeof CodecType];
+
 type PluginCallback = (args: IpluginInputArgs) => IpluginOutputArgs;
 type DetailsCallback = () => IpluginDetails;
+
+const isValidLanguageCode = (code: string): boolean => code.length === 3;
 
 const ffMpegCommandPlugin = (details: DetailsCallback, callback: PluginCallback): PluginCallback => (args) => {
   const lib = require('../../../../methods/lib')();
