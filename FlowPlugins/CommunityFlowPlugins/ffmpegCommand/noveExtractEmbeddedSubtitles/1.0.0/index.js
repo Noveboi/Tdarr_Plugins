86dcdfcfcc589e656fa2c87a349431de662d895e
George Nikolaidis <nove@novesoftware.net>
2026-07-03 11:32:20 +0300

    Add tests for subtitle filtering

---
+++ FlowPlugins/CommunityFlowPlugins/ffmpegCommand/noveExtractEmbeddedSubtitles/1.0.0/index.js
@@ -0,0 +1,75 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.details = exports.plugin = void 0;
+var ffmpeg_1 = require("../../../../FlowHelpers/1.0.0/nove/ffmpeg");
+var types_1 = require("../../../../FlowHelpers/1.0.0/nove/types");
+/* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
+var details = function () { return ({
+    name: 'Extract Embedded Subtitles',
+    description: 'Extract subtitle tracks to separate files',
+    style: {
+        borderColor: '#6efefc',
+    },
+    tags: 'subtitles',
+    isStartPlugin: false,
+    pType: '',
+    requiresVersion: '2.11.01',
+    sidebarPosition: -1,
+    icon: '',
+    inputs: [],
+    outputs: [
+        {
+            number: 1,
+            tooltip: 'Found subtitles and extracted them',
+        },
+        {
+            number: 2,
+            tooltip: 'Did not found any subtitles, did nothing',
+        },
+    ],
+}); };
+exports.details = details;
+var displaySubtitleLanguages = function (streams) { return streams
+    .map(function (s) { var _a, _b; return (_b = (_a = s.tags) === null || _a === void 0 ? void 0 : _a.language) !== null && _b !== void 0 ? _b : '?'; })
+    .join(', '); };
+var createSubtitleFilename = function (fileObj, stream) {
+    var _a;
+    var filename = fileObj.file;
+    var extension = 'srt';
+    var language = (_a = stream.tags) === null || _a === void 0 ? void 0 : _a.language;
+    if (!language) {
+        return (0, types_1.err)('No language defined for subtitle');
+    }
+    var extensionIndex = filename.lastIndexOf('.');
+    var filenameWithoutExtension = extensionIndex === -1
+        ? filename
+        : filename.substring(0, extensionIndex);
+    return (0, types_1.ok)("".concat(filenameWithoutExtension, ".").concat(language, ".").concat(extension));
+};
+var plugin = (0, ffmpeg_1.ffMpegCommandPlugin)(details, function (args) {
+    var subtitleStreams = args.variables.ffmpegCommand.streams
+        .filter(function (s) { return s.codec_type === 'subtitle'; });
+    if (subtitleStreams.length === 0) {
+        args.jobLog('No subtitles found, exiting');
+        return {
+            outputFileObj: args.inputFileObj,
+            outputNumber: 2,
+            variables: args.variables,
+        };
+    }
+    args.jobLog("Found ".concat(subtitleStreams.length, " subtitles to extract: [").concat(displaySubtitleLanguages(subtitleStreams), "]"));
+    subtitleStreams.forEach(function (stream, idx) {
+        var outputFilenameResult = createSubtitleFilename(args.inputFileObj, stream);
+        if (!outputFilenameResult.ok) {
+            args.jobLog("Skipping subtitle #".concat(idx, ", reason: ").concat(outputFilenameResult.error));
+            return;
+        }
+        stream.outputArgs.push('-map 0:s:{outputTypeIndex}', outputFilenameResult.value);
+    });
+    return {
+        outputFileObj: args.inputFileObj,
+        outputNumber: 1,
+        variables: args.variables,
+    };
+});
+exports.plugin = plugin;
---
+++ tests/NovePlugins/noveFilterSubtitles/1.0.0/index.test.ts
@@ -0,0 +1,232 @@
+import { plugin } from '../../../../FlowPluginsTs/CommunityFlowPlugins/ffmpegCommand/noveFilterSubtitles/1.0.0/index';
+import { PluginInputArgsBuilder } from '../../../../FlowPluginsTs/FlowHelpers/1.0.0/nove/pluginHelper';
+
+/*
+Due to the similar nature of the `filterSubtitles` and `filterAudio` plugins,
+the tests here are almost identical. I've intentionally refrained from creating utilities that encapsulate
+common logic between the two plugins in order to keep them completely separate logically for future additions.
+*/
+
+describe('Filtering Subtitle Streams by Language', () => {
+  describe('Standard Behavior', () => {
+    it('should discard unwanted languages when stream with target language exists', () => {
+      const targetLanguage = 'ell';
+
+      const args = new PluginInputArgsBuilder()
+        .withInput('languages', targetLanguage)
+        .addSubtitleStream({ tags: { language: targetLanguage } })
+        .addSubtitleStream({ tags: { language: 'eng' } })
+        .build();
+
+      const output = plugin(args);
+      const { streams } = output.variables.ffmpegCommand;
+
+      expect(output.outputNumber).toBe(1);
+      expect(output.outputFileObj).toBe(args.inputFileObj);
+      expect(streams).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ removed: true, tags: { language: 'eng' } }),
+          expect.objectContaining({ removed: false, tags: { language: targetLanguage } }),
+        ]),
+      );
+    });
+
+    it('should discard multiple unwanted languages when stream with target language exists', () => {
+      const targetLanguage = 'kor';
+
+      const args = new PluginInputArgsBuilder()
+        .withInput('languages', targetLanguage)
+        .addSubtitleStream({ tags: { language: targetLanguage } })
+        .addSubtitleStream({ tags: { language: 'eng' } })
+        .addSubtitleStream({ tags: { language: 'fre' } })
+        .build();
+
+      const output = plugin(args);
+      const { streams } = output.variables.ffmpegCommand;
+
+      expect(output.outputNumber).toBe(1);
+      expect(output.outputFileObj).toBe(args.inputFileObj);
+      expect(streams).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ removed: true, tags: { language: 'fre' } }),
+          expect.objectContaining({ removed: true, tags: { language: 'eng' } }),
+          expect.objectContaining({ removed: false, tags: { language: targetLanguage } }),
+        ]),
+      );
+    });
+
+    // In contrast to `filterAudio`, `filterSubtitles` DOES wipe all subtitles if no matches exist.
+    it('should remove all subtitles when target language does not exist and backup languages are not defined', () => {
+      const args = new PluginInputArgsBuilder()
+        .withInput('languages', 'ell')
+        .addSubtitleStream({ tags: { language: 'eng' } })
+        .build();
+
+      const output = plugin(args);
+      const { streams } = output.variables.ffmpegCommand;
+
+      expect(output.outputNumber).toBe(1);
+      expect(output.outputFileObj).toBe(args.inputFileObj);
+      expect(streams).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ removed: true, tags: { language: 'eng' } }),
+        ]),
+      );
+    });
+
+    it('should accept multiple comma-separated target languages if all target languages exist', () => {
+      const args = new PluginInputArgsBuilder()
+        .withInput('languages', 'jap,ell')
+        .addSubtitleStream({ tags: { language: 'eng' } })
+        .addSubtitleStream({ tags: { language: 'jap' } })
+        .addSubtitleStream({ tags: { language: 'ell' } })
+        .build();
+
+      const output = plugin(args);
+      const { streams } = output.variables.ffmpegCommand;
+
+      expect(output.outputNumber).toBe(1);
+      expect(output.outputFileObj).toBe(args.inputFileObj);
+      expect(streams).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ removed: false, tags: { language: 'jap' } }),
+          expect.objectContaining({ removed: false, tags: { language: 'ell' } }),
+          expect.objectContaining({ removed: true, tags: { language: 'eng' } }),
+        ]),
+      );
+    });
+
+    it('should accept multiple comma-separated target languages if at least one target language exists', () => {
+      const args = new PluginInputArgsBuilder()
+        .withInput('languages', 'jap,ell')
+        .addSubtitleStream({ tags: { language: 'eng' } })
+        .addSubtitleStream({ tags: { language: 'ell' } })
+        .build();
+
+      const output = plugin(args);
+      const { streams } = output.variables.ffmpegCommand;
+
+      expect(output.outputNumber).toBe(1);
+      expect(output.outputFileObj).toBe(args.inputFileObj);
+      expect(streams).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ removed: false, tags: { language: 'ell' } }),
+          expect.objectContaining({ removed: true, tags: { language: 'eng' } }),
+        ]),
+      );
+    });
+  });
+
+  // This is a `filterSubtitles`-specific feature. If some media were to have no matches
+  // for the target languages, some users may still want to have a secondary language available.
+  // Personal Examples:
+  // - In Anime, I want the main subtitle language to be 'Greek', however that's not always available, in that
+  // case I still want subtitles so I resort to 'English' as a backup because I unfortunately don't speak Japanese.
+  describe('Backup Languages', () => {
+    it('should resort to using backup languages if the target languages are not found', () => {
+      const args = new PluginInputArgsBuilder()
+        .withInput('languages', 'ell')
+        .withInput('backupLanguages', 'eng')
+        .addSubtitleStream({ tags: { language: 'eng' } })
+        .addSubtitleStream({ tags: { language: 'fre' } })
+        .build();
+
+      const output = plugin(args);
+      const { streams } = output.variables.ffmpegCommand;
+
+      expect(output.outputNumber).toBe(1);
+      expect(output.outputFileObj).toBe(args.inputFileObj);
+      expect(streams).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ removed: true, tags: { language: 'fre' } }),
+          expect.objectContaining({ removed: false, tags: { language: 'eng' } }),
+        ]),
+      );
+    });
+
+    it('should not use backup languages if target languages exist', () => {
+      const args = new PluginInputArgsBuilder()
+        .withInput('languages', 'ell')
+        .withInput('backupLanguages', 'eng')
+        .addSubtitleStream({ tags: { language: 'ell' } })
+        .addSubtitleStream({ tags: { language: 'eng' } })
+        .addSubtitleStream({ tags: { language: 'fre' } })
+        .build();
+
+      const output = plugin(args);
+      const { streams } = output.variables.ffmpegCommand;
+
+      expect(output.outputNumber).toBe(1);
+      expect(output.outputFileObj).toBe(args.inputFileObj);
+      expect(streams).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ removed: false, tags: { language: 'ell' } }),
+          expect.objectContaining({ removed: true, tags: { language: 'fre' } }),
+          expect.objectContaining({ removed: true, tags: { language: 'eng' } }),
+        ]),
+      );
+    });
+
+    it('should still discard all subtitles if no target/backup languages exist', () => {
+      const args = new PluginInputArgsBuilder()
+        .withInput('languages', 'ell')
+        .withInput('backupLanguages', 'eng')
+        .addSubtitleStream({ tags: { language: 'chi' } })
+        .build();
+
+      const output = plugin(args);
+      const { streams } = output.variables.ffmpegCommand;
+
+      expect(output.outputNumber).toBe(1);
+      expect(output.outputFileObj).toBe(args.inputFileObj);
+      expect(streams).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ removed: true, tags: { language: 'chi' } }),
+        ]),
+      );
+    });
+  });
+
+  describe('Edge Cases', () => {
+    it('should trim spaces between commas in `languages` list', () => {
+      const args = new PluginInputArgsBuilder()
+        .withInput('languages', ' eng, ell,jap,     fre')
+        .addSubtitleStream({ tags: { language: 'eng' } })
+        .addSubtitleStream({ tags: { language: 'ell' } })
+        .addSubtitleStream({ tags: { language: 'jap' } })
+        .addSubtitleStream({ tags: { language: 'fre' } })
+        .addSubtitleStream({ tags: { language: 'esp' } })
+        .build();
+
+      const output = plugin(args);
+      const { streams } = output.variables.ffmpegCommand;
+
+      expect(output.outputNumber).toBe(1);
+      expect(output.outputFileObj).toBe(args.inputFileObj);
+      expect(streams).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ removed: true, tags: { language: 'esp' } }),
+          expect.objectContaining({ removed: false, tags: { language: 'eng' } }),
+          expect.objectContaining({ removed: false, tags: { language: 'ell' } }),
+          expect.objectContaining({ removed: false, tags: { language: 'jap' } }),
+          expect.objectContaining({ removed: false, tags: { language: 'fre' } }),
+        ]),
+      );
+    });
+  });
+
+  describe('Invalid Usage', () => {
+    it('should throw when "languages" input is not defined', () => {
+      const args = new PluginInputArgsBuilder().build();
+      expect(() => plugin(args)).toThrow(/empty.*specify.*language/i);
+    });
+
+    it('should throw when "languages" contains one or more invalid languages codes', () => {
+      const args = new PluginInputArgsBuilder()
+        .withInput('languages', 'English, Greek')
+        .build();
+
+      expect(() => plugin(args)).toThrow(/english.*greek/i);
+    });
+  });
+});
